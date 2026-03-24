acb1b91da59d2279311fc3c6b30694d055ab3770
Tobias Moscatelli <moscatellitobias@gmail.com>
2026-03-23 22:30:51 -0300

    fix: fix to next aws cache

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,28 @@ const nextConfig: NextConfig = {
   env: {
     APP_VERSION: process.env.npm_package_version,
   },
-  images: {
+ images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        pathname: '/**',
+      }
     ],
   },
   allowedDevOrigins: ['galvanoscopic-lauretta-plurally.ngrok-free.dev'],
