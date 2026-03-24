5e51958cacc6b0584ca2d773eaa7ef3621a21c7c
Tobias Moscatelli <moscatellitobias@gmail.com>
2026-03-24 18:10:50 -0300

    fix: fixed div movement on loading

--- app/components/game-background/ClientGameBackground.tsx
+++ app/components/game-background/ClientGameBackground.tsx
@@ -3,7 +3,21 @@ import dynamic from 'next/dynamic'
 
 const DynamicGame = dynamic(
   () => import('./GameBackground').then(mod => mod.GameBackground), 
-  { ssr: false, loading: () => <div style={{ background: '#090A0F', height: '100vh' }} /> }
+  { 
+    ssr: false, 
+    loading: () => (
+      <div 
+        style={{ 
+          position: 'fixed', 
+          inset: 0,
+          background: '#090A0F', 
+          zIndex: -1 
+        }} 
+      />
+    ) 
+  }
 )
 
-export function ClientGameBackground() { return <DynamicGame /> }
+export function ClientGameBackground() {
+  return <DynamicGame />
+}
