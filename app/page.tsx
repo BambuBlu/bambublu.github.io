9d4b4f0b232d55c750bace7388c74cf2da3a7cce
Tobias Moscatelli <moscatellitobias@gmail.com>
2026-03-26 23:55:26 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { BottomMenu, ViewRenderer, Header, ClientGameBackground } from "./components"
 
-export default function Home() {
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
   return (
     <main>
       <ClientGameBackground />
